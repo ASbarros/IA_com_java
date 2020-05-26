a1abd3b63b38ba3990b302258127a53754b92d59
asbarros <anderson.barros0298@gmail.com>
2020-05-26 00:09:29 -0300

    refatorando

--- demo/src/main/java/anderson/Recomendados.java
+++ demo/src/main/java/anderson/Recomendados.java
@@ -1,11 +1,11 @@
-package demo.java.anderson;
+package anderson;
 
 import java.io.File;
 import java.io.IOException;
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
 import org.apache.mahout.cf.taste.model.DataModel;
 
-public class Recomendador {
+public class Recomendados {
 
   public DataModel getModeloLivros() throws IOException {
     return getModelo("livros.csv");
